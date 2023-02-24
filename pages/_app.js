30171435622892a11dd800fcb92aaf55891d2a24
artify12 <thedudeserc@gmail.com>
2023-02-24 21:21:13 +0100

    app

--- pages/_app.js
+++ pages/_app.js
@@ -7,10 +7,25 @@ import "../styles/globals.css";
 import 'react-toastify/dist/ReactToastify.css';
 
 import { ToastContainer } from 'react-toastify'
+import Head from 'next/head'
+import pkg from "../package.json";
+const HOST = process.env.VERCEL_URL
+  ? `https://${process.env.VERCEL_URL}`
+  : "http://localhost:3000";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <meta name="description" content={pkg.appMetaDescription} />
+        <meta property="og:title" content={pkg.appName} />
+        <meta property="og:description" content={pkg.appMetaDescription} />
+        <meta
+          property="og:image"
+          content={`${HOST}/preview.png`}
+        />
+        <title>{pkg.appName}</title>
+      </Head>
       <Component {...pageProps} />
       <ToastContainer />
     </>
